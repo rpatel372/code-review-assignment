d8d41ffd760cd4df61fc53d3fd2d983cb07490ad
Reema Patel <reemapatel97@gmail.com>
2022-03-26 14:16:19 -0400

    Test Bitbucket hook

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/incoming-pr', (req, res) => {
+  res.send('Hello PR')
+})
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
