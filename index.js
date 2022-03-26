7b4551fc7de625717ee40096f2c8401c57c06186
Reema Patel <reemapatel97@gmail.com>
2022-03-26 14:27:33 -0400

    Change bitbucket webhook to post request

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/incoming-pr', (req, res) => {
-  res.send('Hello PR')
-})
+app.post('/incoming-pr',(request,response) => {
+    res.send(request.body);
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
